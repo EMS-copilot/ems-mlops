92100d37b6e1bcac26f41411948024144e777ee5
esillileu <esillileu@gmail.com>
2025-10-23 16:11:57 +0900

    rm batch info - predictor integration

--- codes/__init__.py
+++ codes/__init__.py
@@ -1,4 +1,4 @@
-from codes.preprocess import custom_preprocess
-from codes.static_resource import StaticResource
-from codes.batch_info import BatchInfo
+from codes.preprocess import custom_preprocess, parse_batch_info
+from codes.static_resources import StaticResources
 from codes.logger_config import setup_logging
+from codes.model_loader import load_infer
