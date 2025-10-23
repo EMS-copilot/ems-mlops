536f6e4c25c4e90b2fd4c23ab5f7c8b5a5f5cd54
esillileu <esillileu@gmail.com>
2025-10-23 17:09:47 +0900

    ref input schema

--- codes/schemas/__init__.py
+++ codes/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .patient import Patient
 from .hospital import Hospital
-from .input import InputSchema
+from .input_schema import InputSchema
 
 from pydantic import BaseModel
 from typing import List
